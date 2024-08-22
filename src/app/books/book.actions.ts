bef1f3e3a8730cbd51c6018df0a4f40bbb192ce2
Kevin Fang <kevin-fang@hotmail.ca>
2024-08-22 17:07:25 -0400

    add initial actions and reducers

---
+++ src/app/books/book.actions.ts
@@ -0,0 +1,20 @@
+import { createAction, props } from "@ngrx/store";
+import { Book } from "../models/book";
+
+/* 
+Actions: describe unique events that are dispatched from components and services.
+---------------------------------------
+First Parameter: [Source] Action Type
+
+This is a string that uniquely identifies the action. It usually follows the format [Source] Action Type, where:
+This string does not provide any functionality by itself; it is used to uniquely identify the action type.
+---------------------------------------
+Second Parameter: props<>() Function
+
+The props<>() function is used to define the payload of the action. It specifies the shape of the data that the action will carry.
+The props function takes a generic type parameter that defines the structure of the payload. For example, props<{ payload: any }>() indicates that the action will carry an object with a payload property of type any.
+---------------------------------------
+*/
+
+export const AddBook = createAction('[Book] Add Book', props<Book>());
+export const RemoveBook = createAction('[Book] Remove Book', props<{bookId: string}>());
---
+++ src/app/books/books.reducers.ts
@@ -0,0 +1,20 @@
+import { createReducer, on } from "@ngrx/store";
+import { AddBook, RemoveBook } from "./book.actions";
+import { Book } from "../models/book";
+
+/* 
+Reducers: Reducers copy current state and apply a dispatched action to the copy to create a new state.
+---------------------------------------
+
+---------------------------------------
+*/
+
+// Reducers always need an initial state    
+export const initalState: ReadonlyArray<Book> = [];
+
+export const BookReducer =  createReducer(
+    initalState,
+    // when AddBook action is dispatched, take the current state,  create a new array with the new book added to it, and return the new array
+    on(AddBook, (state, { id, title, author}) => [...state, {id, title, author}]),
+    on(RemoveBook, (state, {bookId}) => state.filter(book => book.id !== bookId))
+)
